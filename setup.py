0893a54413e28156e22f8b5b7df68e3cb29f9433
qilei123 <qileimail123@gmail.com>
2024-08-08 17:54:51 -0400

    for release

---
+++ setup.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+from setuptools import setup,find_packages
+
+
+setup(
+        name='y9inference',
+        version='1.0', 
+        description='yolov9 for gastroscopy to detect esophageal cancer',
+        author='highwise_research', 
+        author_email='qileimail123@gmail.com', 
+        packages=find_packages(),
+)
